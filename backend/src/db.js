e8f6980df2ebf1a202ef311d82c2781e7ab252c6
SimonG <saguti15@g.holycross.edu>
2023-11-21 16:00:26 -0500

    SG: worked on set up and have a working app

--- backend/src/db.js
+++ backend/src/db.js
@@ -1,11 +1,11 @@
-const Pool = require("pg").Pool;
+const Pool = require('pg').Pool;
 
 const pool = new Pool({
-  user: "postgres",
-  password: "xavimessi1",
-  host: "localhost",
+  user: 'simongutierrez',
+  password: 'xavimessi1',
+  host: 'localhost',
   port: 5432,
-  database: "perntodo"
+  database: 'perntodo'
 });
 
 module.exports = pool;
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,7 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const cors = require("cors");
-const pool = require("./db");
+const cors = require('cors');
+const pool = require('./db');
 
 //middleware
 app.use(cors());
@@ -11,11 +11,11 @@ app.use(express.json()); //req.body
 
 //create a todo
 
-app.post("/todos", async (req, res) => {
+app.post('/todos', async (req, res) => {
   try {
     const { description } = req.body;
     const newTodo = await pool.query(
-      "INSERT INTO todo (description) VALUES($1) RETURNING *",
+      'INSERT INTO todo (description) VALUES($1) RETURNING *',
       [description]
     );
 
@@ -27,9 +27,9 @@ app.post("/todos", async (req, res) => {
 
 //get all todos
 
-app.get("/todos", async (req, res) => {
+app.get('/todos', async (req, res) => {
   try {
-    const allTodos = await pool.query("SELECT * FROM todo");
+    const allTodos = await pool.query('SELECT * FROM todo');
     res.json(allTodos.rows);
   } catch (err) {
     console.error(err.message);
@@ -38,10 +38,10 @@ app.get("/todos", async (req, res) => {
 
 //get a todo
 
-app.get("/todos/:id", async (req, res) => {
+app.get('/todos/:id', async (req, res) => {
   try {
     const { id } = req.params;
-    const todo = await pool.query("SELECT * FROM todo WHERE todo_id = $1", [
+    const todo = await pool.query('SELECT * FROM todo WHERE todo_id = $1', [
       id
     ]);
 
@@ -53,16 +53,16 @@ app.get("/todos/:id", async (req, res) => {
 
 //update a todo
 
-app.put("/todos/:id", async (req, res) => {
+app.put('/todos/:id', async (req, res) => {
   try {
     const { id } = req.params;
     const { description } = req.body;
     const updateTodo = await pool.query(
-      "UPDATE todo SET description = $1 WHERE todo_id = $2",
+      'UPDATE todo SET description = $1 WHERE todo_id = $2',
       [description, id]
     );
 
-    res.json("Todo was updated!");
+    res.json('Todo was updated!');
   } catch (err) {
     console.error(err.message);
   }
@@ -70,18 +70,18 @@ app.put("/todos/:id", async (req, res) => {
 
 //delete a todo
 
-app.delete("/todos/:id", async (req, res) => {
+app.delete('/todos/:id', async (req, res) => {
   try {
     const { id } = req.params;
-    const deleteTodo = await pool.query("DELETE FROM todo WHERE todo_id = $1", [
+    const deleteTodo = await pool.query('DELETE FROM todo WHERE todo_id = $1', [
       id
     ]);
-    res.json("Todo was deleted!");
+    res.json('Todo was deleted!');
   } catch (err) {
     console.log(err.message);
   }
 });
 
 app.listen(5000, () => {
-  console.log("server has started on port 5000");
+  console.log('server has started on port 5000');
 });
